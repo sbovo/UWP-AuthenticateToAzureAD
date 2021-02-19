0b3707a1bd8b3b837973c214213d0d231f9c1cfd
Sebastien Bovo <sbovo@microsoft.com>
2021-02-19 14:27:29 +0100

    Simple UWP

---
+++ AuthenticateToAzureAD.UWP/AuthenticateToAzureAD.UWP/MainPage.xaml.cs
@@ -0,0 +1,177 @@
+﻿using Microsoft.Identity.Client;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+
+
+
+namespace AuthenticateToAzureAD.UWP
+{
+
+    public sealed partial class MainPage : Page
+    {
+
+        //Set the scope for API call to user.read
+        private string[] scopes = new string[] { "user.read" };
+
+        // Below are the clientId (Application Id) of your app registration and the tenant information.
+        // You have to replace:
+        // - the content of ClientID with the Application Id for your app registration
+        private const string ClientId = "clientid";
+
+        private const string Tenant = "tenantid"; // Alternatively "[Enter your tenant, as obtained from the Azure portal, e.g. kko365.onmicrosoft.com]"
+        private const string Authority = "https://login.microsoftonline.com/" + Tenant;
+
+        // The MSAL Public client app
+        private static IPublicClientApplication PublicClientApp;
+
+        private static string MSGraphURL = "https://graph.microsoft.com/v1.0/";
+        private static AuthenticationResult authResult;
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+
+
+        /// <summary>
+        /// Sign in the user
+        /// </summary>
+        private async void btnSignIn_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                // Sign in user using MSAL and obtain an access token
+                string stringToken = await SignInUserAndGetTokenUsingMSAL(scopes);
+				await Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, () =>
+				{
+					this.btnSignIn.Visibility = Visibility.Collapsed;
+					this.btnSignOut.Visibility = Visibility.Visible;
+				});
+			}
+            catch (MsalException msalEx)
+            {
+                await DisplayMessageAsync($"Error Acquiring Token:{System.Environment.NewLine}{msalEx}");
+            }
+            catch (Exception ex)
+            {
+                await DisplayMessageAsync($"Error {System.Environment.NewLine}{ex}");
+                return;
+            }
+        }
+
+
+        /// <summary>
+        /// Sign out the current user
+        /// </summary>
+        private async void btnSignOut_Click(object sender, RoutedEventArgs e)
+        {
+            IEnumerable<IAccount> accounts = await PublicClientApp.GetAccountsAsync().ConfigureAwait(false);
+            IAccount firstAccount = accounts.FirstOrDefault();
+
+            try
+            {
+                await PublicClientApp.RemoveAsync(firstAccount).ConfigureAwait(false);
+                await Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, () =>
+                {
+                    this.btnSignIn.Visibility = Visibility.Visible;
+                    this.btnSignOut.Visibility = Visibility.Collapsed;
+                });
+                await DisplayMessageAsync("User has signed out");
+            }
+            catch (MsalException ex)
+            {
+                await DisplayMessageAsync($"Error signing out user: {ex.Message}");
+            }
+        }
+
+
+
+
+
+
+        /// <summary>
+        /// Signs in the user and obtains an access token
+        /// </summary>
+        /// <param name="scopes"></param>
+        /// <returns> Access Token</returns>
+        private async Task<string> SignInUserAndGetTokenUsingMSAL(string[] scopes)
+        {
+            // Initialize the MSAL library by building a public client application
+            PublicClientApp = PublicClientApplicationBuilder.Create(ClientId)
+                .WithAuthority(Authority)
+                .WithUseCorporateNetwork(false)
+                .WithRedirectUri("https://login.microsoftonline.com/common/oauth2/nativeclient")
+                 .WithLogging((level, message, containsPii) =>
+                 {
+                     Debug.WriteLine($"MSAL: {level} {message} ");
+                 }, LogLevel.Warning, enablePiiLogging: false, enableDefaultPlatformLogging: true)
+                .Build();
+
+            // It's good practice to not do work on the UI thread, so use ConfigureAwait(false) whenever possible.
+            IEnumerable<IAccount> accounts = await PublicClientApp.GetAccountsAsync().ConfigureAwait(false);
+            IAccount firstAccount = accounts.FirstOrDefault();
+
+            try
+            {
+                authResult = await PublicClientApp.AcquireTokenSilent(scopes, firstAccount)
+                                                  .ExecuteAsync();
+            }
+            catch (MsalUiRequiredException ex)
+            {
+                // A MsalUiRequiredException happened on AcquireTokenSilentAsync. 
+                // This indicates you need to call AcquireTokenAsync to acquire a token
+                Debug.WriteLine($"MsalUiRequiredException: {ex.Message}");
+
+                authResult = await PublicClientApp.AcquireTokenInteractive(scopes)
+                                                  .ExecuteAsync()
+                                                  .ConfigureAwait(false);
+
+            }
+
+            // Displays basic information contained in the token
+            await DisplayMessageAsync(
+                $"User Name: {authResult.Account.Username}"
+                + Environment.NewLine +
+                $"Token Expires: {authResult.ExpiresOn.ToLocalTime()}");
+
+
+
+            return authResult.AccessToken;
+        }
+
+
+
+
+
+
+        /// <summary>
+        /// Displays a message in the ResultText. Can be called from any thread.
+        /// </summary>
+        private async Task DisplayMessageAsync(string message)
+        {
+            await Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal,
+                () =>
+                {
+                    textblockResultText.Text = message;
+                });
+        }
+
+
+    }
+}
